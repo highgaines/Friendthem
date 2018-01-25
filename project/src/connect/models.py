6dfe48d9a7e923218ab914ef520f41ebd684f43e
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-01-25 16:40:09 -0200

    Add connect app with connection model.

---
+++ project/src/connect/models.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+class Connection(models.Model):
+    FACEBOOK = 'facebook'
+    INSTAGRAM = 'instagram'
+    TWITTER = 'Twitter'
+    YOUTUBE = 'youtube'
+
+    PROVIDER_CHOICES = (
+        (FACEBOOK, 'Facebook'),
+        (INSTAGRAM, 'Instagram'),
+        (YOUTUBE, 'Youtube'),
+        (TWITTER, 'Twitter')
+    )
+
+    user_1 = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='connection_user_1',
+        on_delete=models.CASCADE
+    )
+    user_2 = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='connection_user_2',
+        on_delete=models.CASCADE
+    )
+    provider = models.CharField(max_length=10, choices=PROVIDER_CHOICES)
+    confirmed = models.BooleanField(default=True)
--- project/src/settings.py
+++ project/src/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'social_django',
     'rest_framework_social_oauth2',
     'src.core_auth',
+    'src.connect',
 ]
 
 MIDDLEWARE = [

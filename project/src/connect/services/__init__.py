eef926c1498231c87bd19afdcbab7eca4d2a3fdc
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-01-29 18:43:12 -0200

    Add DummyConnect class to serve as default to serializer.

--- project/src/connect/services/__init__.py
+++ project/src/connect/services/__init__.py
@@ -1,3 +1,4 @@
+from src.connect.services.dummy import DummyConnect
 from src.connect.services.instagram import InstagramConnect
 from src.connect.services.twitter import TwitterConnect
 from src.connect.services.youtube import YoutubeConnect

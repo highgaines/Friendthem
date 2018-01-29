beeff11ad650a2527dbd3c8098eb02fab2f9fb3a
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-01-29 13:51:50 -0200

    Add instagram connect client to connect app.

--- project/src/connect/services.py
+++ project/src/connect/services.py
@@ -1,11 +1,12 @@
 import twitter
+from instagram import InstagramAPI, InstagramClientError
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 
-class TwitterCredentialsNotFound(Exception):
+class CredentialsNotFound(Exception):
     pass
 
-class TwitterUserNotFound(Exception):
+class SocialUserNotFound(Exception):
     pass
 
 class TwitterConnect(object):
@@ -18,7 +19,7 @@ class TwitterConnect(object):
             token_key = social_auth.extra_data.get('access_token', {})['oauth_token']
             token_secret =  social_auth.extra_data.get('access_token', {})['oauth_token_secret']
         except (ObjectDoesNotExist, KeyError):
-            raise TwitterCredentialsNotFound
+            raise CredentialsNotFound
 
         return twitter.Api(
             consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
@@ -30,14 +31,42 @@ class TwitterConnect(object):
         try:
             other_social_auth = other_user.social_auth.get(provider='twitter')
             other_user_id = other_social_auth.uid
-            if not other_user_id:
-                raise ObjectDoesNotExist
         except ObjectDoesNotExist:
-            raise TwitterUserNotFound
+            raise SocialUserNotFound
 
         friendship = self.api.CreateFriendship(user_id=other_user_id, follow=True)
 
 
         if friendship.following:
+            return True
+        return False
+
+
+class InstagramConnect(object):
+    def __init__(self, user):
+        self.api = self._authenticate(user)
+
+    def _authenticate(self, user):
+        try:
+            social_auth = user.social_auth.get(provider='instagram')
+            token_key = social_auth.extra_data['access_token']
+        except (ObjectDoesNotExist, KeyError):
+            raise CredentialsNotFound
+
+        return InstagramAPI(
+            client_secret=settings.SOCIAL_AUTH_INSTAGRAM_SECRET,
+            access_token=token_key
+        )
+
+    def connect(self, other_user):
+        try:
+            other_social_auth = other_user.social_auth.get(provider='instagram')
+            other_user_id = other_social_auth.uid
+        except ObjectDoesNotExist:
+            raise SocialUserNotFound
+
+        follow = self.api.follow_user(user_id=other_user_id)
+
+        if follow[0].outgoing_status == 'follows':
             return True
         return False
--- project/src/connect/tests/test_services.py
+++ project/src/connect/tests/test_services.py
@@ -4,7 +4,7 @@ from model_mommy import mommy
 
 from django.test import TestCase
 from django.conf import settings
-from src.connect.services import TwitterConnect, TwitterCredentialsNotFound, TwitterUserNotFound
+from src.connect.services import TwitterConnect, InstagramConnect, CredentialsNotFound, SocialUserNotFound
 
 class TwitterConnectTestCase(TestCase):
     def setUp(self):
@@ -39,13 +39,13 @@ class TwitterConnectTestCase(TestCase):
 
     def test_authenticate_raises_error_if_no_user_social_auth(self):
         self.user_social_auth.delete()
-        with pytest.raises(TwitterCredentialsNotFound):
+        with pytest.raises(CredentialsNotFound):
             connect = TwitterConnect(self.user)
 
     def test_authenticate_raises_error_if_no_user_credentials(self):
         self.user_social_auth.extra_data = {}
         self.user_social_auth.save()
-        with pytest.raises(TwitterCredentialsNotFound):
+        with pytest.raises(CredentialsNotFound):
             connect = TwitterConnect(self.user)
 
     @patch('src.connect.services.twitter')
@@ -74,6 +74,77 @@ class TwitterConnectTestCase(TestCase):
         self.other_social_auth.delete()
 
         connect = TwitterConnect(self.user)
-        with pytest.raises(TwitterUserNotFound):
+        with pytest.raises(SocialUserNotFound):
             connection = connect.connect(self.other_user)
         api_object.CreateFriendship.assert_not_called()
+
+
+class InstagramConnectTestCase(TestCase):
+    def setUp(self):
+        self.user_social_auth = mommy.make(
+            'UserSocialAuth',
+            provider='instagram',
+            extra_data={'access_token': '123456'}
+        )
+        self.user = self.user_social_auth.user
+
+        self.other_social_auth = mommy.make(
+            'UserSocialAuth',
+            provider='instagram',
+            uid='123456'
+        )
+        self.other_user = self.other_social_auth.user
+
+    @patch.object(InstagramConnect, '_authenticate')
+    def test_initialization_calls_authenticate_for_user_1(self, authenticate):
+        connect = InstagramConnect(self.user)
+        authenticate.assert_called_once_with(self.user)
+
+    @patch('src.connect.services.InstagramAPI')
+    def test_authenticate_calls_api_with_tokens(self, mocked_instagram):
+        connect = InstagramConnect(self.user)
+        mocked_instagram.assert_called_once_with(
+            client_secret=settings.SOCIAL_AUTH_INSTAGRAM_SECRET,
+            access_token='123456'
+        )
+
+    def test_authenticate_raises_error_if_no_user_social_auth(self):
+        self.user_social_auth.delete()
+        with pytest.raises(CredentialsNotFound):
+            connect = InstagramConnect(self.user)
+
+    def test_authenticate_raises_error_if_no_user_credentials(self):
+        self.user_social_auth.extra_data = {}
+        self.user_social_auth.save()
+        with pytest.raises(CredentialsNotFound):
+            connect = InstagramConnect(self.user)
+
+    @patch('src.connect.services.InstagramAPI')
+    def test_connect_calls_create_friendship_on_twitter(self, mocked_instagram):
+        api_object = Mock()
+        follow = [Mock()]
+        follow[0].outgoing_status = 'follows'
+        api_object.follow_user.return_value = follow
+        mocked_instagram.return_value = api_object
+
+        connect = InstagramConnect(self.user)
+        connection = connect.connect(self.other_user)
+        assert connection is True
+        api_object.follow_user.assert_called_once_with(
+            user_id=self.other_social_auth.uid,
+        )
+
+    @patch('src.connect.services.twitter')
+    def test_connect_raises_error_if_user_social_auth_does_not_exist(self, mocked_instagram):
+        api_object = Mock()
+        follow = [Mock()]
+        follow[0].outgoing_status = 'follows'
+        api_object.follow_user.return_value = follow
+        mocked_instagram.return_value = api_object
+
+        self.other_social_auth.delete()
+
+        connect = InstagramConnect(self.user)
+        with pytest.raises(SocialUserNotFound):
+            connection = connect.connect(self.other_user)
+        api_object.follow_user.assert_not_called()

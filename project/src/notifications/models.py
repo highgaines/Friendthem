d7da0696d1f2daedbe96b2abd66510c4e11f3db0
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-02-14 11:53:36 -0200

    Create Device model.

--- project/src/notifications/models.py
+++ project/src/notifications/models.py
@@ -1,3 +1,6 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    device_id = models.CharField(max_length=64)

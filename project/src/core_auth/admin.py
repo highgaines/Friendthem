010d121500bee92ec25617b9fbfdfb9d1e690c92
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-02-20 14:09:03 -0300

    Use widget for user last location on admin.

--- project/src/core_auth/admin.py
+++ project/src/core_auth/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.gis.db import models
 
-# Register your models here.
+from mapwidgets.widgets import GooglePointFieldWidget
+
+User = get_user_model()
+class UserAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.PointField: {"widget": GooglePointFieldWidget}
+    }
+
+admin.site.register(User, UserAdmin)

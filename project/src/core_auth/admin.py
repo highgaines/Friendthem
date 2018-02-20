2e1c8f98c6e74655e8fef8705678aaa12fd87b7c
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-02-20 14:28:00 -0300

    Use fieldsets to display grouped fields.

--- project/src/core_auth/admin.py
+++ project/src/core_auth/admin.py
@@ -6,6 +6,15 @@ from mapwidgets.widgets import GooglePointFieldWidget
 
 User = get_user_model()
 class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('User Data', {'fields': ('email', 'first_name', 'last_name')}),
+        ('Profile', {'fields': (
+            'picture', 'hobbies', 'hometown', 'occupation', 'phone_number', 'age',
+            'personal_email'
+        )}),
+        ('Settings', {'fields': (('ghost_mode', 'notifications'),)}),
+        ('Location', {'fields': ('last_location',)})
+    )
     formfield_overrides = {
         models.PointField: {"widget": GooglePointFieldWidget}
     }

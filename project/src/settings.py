d9ee310b0277c686c6d639df2728b18cbbc59e2f
Henry Xie <henry@simplefractal.com>
2018-02-22 12:14:27 -0500

    use Unipath for BASE_DIR

--- project/src/settings.py
+++ project/src/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import dj_database_url
 from decouple import config, Csv
+from unipath import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).ancestor(2)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/

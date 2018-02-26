9765e47029511928a6595e168c75c5d73d60548d
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-02-26 14:48:38 -0300

    Add delete notification view.

--- project/src/notifications/views.py
+++ project/src/notifications/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render
 from src.notifications.serializers import DeviceSerializer, NotificationSerializer
@@ -14,5 +14,13 @@ class ListNotificationsView(ListAPIView):
     def get_queryset(self):
         return self.request.user.received_notifications.all()
 
+class DeleteNotificationView(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        return self.request.user.received_notifications.all()
+
 add_device_view = AddDeviceView.as_view()
 notifications_view = ListNotificationsView.as_view()
+delete_notification_view = DeleteNotificationView.as_view()

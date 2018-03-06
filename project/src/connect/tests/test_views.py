52811ca20dfc5e8aa218fd20e63a3c5f925acc06
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-03-06 14:57:56 -0300

    Add view to get connected user for the authenticated user.

--- project/src/connect/tests/test_views.py
+++ project/src/connect/tests/test_views.py
@@ -26,3 +26,29 @@ class ConnectionAPIViewTestCase(APITestCase):
         view_class.serializer_class = ConnectionSerializer
         assert issubclass(view_class, CreateAPIView)
 
+
+class ConnectedUsersAPIViewTestCase(APITestCase):
+    def setUp(self):
+        self.user = mommy.make(User)
+        self.connected_user = mommy.make(User)
+        mommy.make('SocialProfile', user=self.user, _quantity=4)
+        mommy.make('SocialProfile', user=self.connected_user, _quantity=4)
+        not_connected_user = mommy.make(User)
+        connection = mommy.make('Connection', user_1=self.user, user_2=self.connected_user, _quantity=2)
+        self.client.force_authenticate(self.user)
+        self.url = reverse('connect:users')
+
+    def test_login_required(self):
+        self.client.logout()
+        response = self.client.get(self.url)
+        assert 401 == response.status_code
+
+    def test_list_connected_users(self):
+        response = self.client.get(self.url)
+        assert 200 == response.status_code
+        content = response.json()
+        assert 1 == len(content)
+        user_data = content[0]
+        assert user_data['id'] == self.connected_user.id
+        assert 50 == user_data['connection_percentage']
+
--- project/src/connect/urls.py
+++ project/src/connect/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from src.connect import views
 
 urlpatterns = [
+    path('users/', views.connected_users_view, name='users'),
     path('', views.connection_view, name='connect'),
 ]

5fbf3ac1190e834f1c9d90b148b9b0a2536dcbc4
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-03-13 13:31:05 -0300

    Add user as connect class attribute.

--- project/src/connect/services/base.py
+++ project/src/connect/services/base.py
@@ -1,6 +1,7 @@
 class BaseConnect(object):
     def __init__(self, user):
-        self.api = self._authenticate(user)
+        self.user = user
+        self.api = self._authenticate(self.user)
 
     def _authenticate(self):
         raise NotImplemented

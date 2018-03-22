26e32138882d2b22742da3957dbe1bda251afd95
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-03-22 14:01:15 -0300

    Move feed check outside of exception handling.

--- project/src/feed/views.py
+++ project/src/feed/views.py
@@ -26,16 +26,17 @@ class FeedView(APIView):
         try:
             service = Service(self.request.user)
             data = service.get_feed(other_user)
-            if not data:
-                connected = Connection.objects.filter(
-                    provider=provider,
-                    user_1=self.request.user,
-                    user_2=other_user).exists()
-                if not connected:
-                    msg = f'User {other_user.id} has private feed and is not connected.'
-                    return Response({'error': msg}, status=status.HTTP_400_BAD_REQUEST)
         except (SocialUserNotFound, CredentialsNotFound):
             return Response({'error', str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not data:
+            connected = Connection.objects.filter(
+                provider=provider,
+                user_1=self.request.user,
+                user_2=other_user).exists()
+            if not connected:
+                msg = f'User {other_user.id} has private feed and is not connected.'
+                return Response({'error': msg}, status=status.HTTP_400_BAD_REQUEST)
 
         data = sorted(data, key=lambda x: x.get('created_time', 0), reverse=True)
         return Response({'data': data, 'user_id': user_id})

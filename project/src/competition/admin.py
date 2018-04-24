fc70b35332240cf48e38bf2417b2428d05682da9
Eduardo Carvalho <eduardooc.86@gmail.com>
2018-04-24 16:00:43 -0300

    Make scoring fields read only

--- project/src/competition/admin.py
+++ project/src/competition/admin.py
@@ -21,12 +21,14 @@ class CollegeCompetitionAdmin(BaseCompetitionAdmin):
         '_full_name', 'date_joined', 'social_sync_points', 'friendthem_points',
         'fraternity_points', 'sorority_points', 'total_points'
     )
+    readonly_fields = list_display
 
 class CompetitionAdmin(BaseCompetitionAdmin):
     list_display = (
         '_full_name', 'date_joined', 'social_sync_points', 'invitations_points',
         'received_connections_points', 'sent_connections_points', 'total_points'
     )
+    readonly_fields = list_display
 
 class CompetitionAdminSite(admin.AdminSite):
     site_header = 'Friendthem Superconnect Competition'
